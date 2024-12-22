557e5bfc6787a841587a7b93698d0ef834eac3b9
Ahmad Raza <ahmedrazawarriach786@gmail.com>
2024-12-22 22:20:57 +0500

    Header And Cart

--- Frontend/src/pages/Cart.tsx
+++ Frontend/src/pages/Cart.tsx
@@ -1,11 +1,62 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { VscError } from "react-icons/vsc";
+
+const cartItems = [];
+const Subtotal = 4000;
+const tax = Math.round(Subtotal * 0.18);
+const shippingCharges = 200;
+const total = Subtotal + tax + shippingCharges;
+const discount = 400;
 
 const Cart = () => {
+  const [cuponCode, setCuponCode] = useState<string>("");
+  const [isValidCuponCode, setIsValidCuponCode] = useState<boolean>(false);
+  useEffect(() => {
+    const timeOutId = setTimeout(() => {
+      if (Math.random() > 0.5) {
+        setIsValidCuponCode(true);
+      } else {
+        setIsValidCuponCode(false);
+      }
+    }, 1000);
+  
+    return () => {
+      clearTimeout(timeOutId);
+      setIsValidCuponCode(false);
+    };
+  }, [cuponCode]);
+  
+  
   return (
-    <div>
-      search
-    </div>
-  )
-}
+    <div className="cart">
+      <main></main>
+      <aside>
+        <p>Subtotal : Rs.{Subtotal}</p>
+        <p>Shipping Charges : Rs.{shippingCharges}</p>
+        <p>Tax : Rs.{tax}</p>
+        <p>
+          Discount : <em> - Rs.{discount}</em>
+        </p>
+        <b>Total:Rs.{total}</b>
+        <input
+          type="text"
+          value={cuponCode}
+          placeholder="Cupon Code"
+          onChange={(e) => setCuponCode(e.target.value)}
+        />
 
-export default Cart
+        {cuponCode && ( isValidCuponCode ? (
+          <span className="green">
+            Rs.{discount} off using the <code>{cuponCode}</code>
+          </span>
+        ) : (
+          <span className="red">
+            Invalid Cupon <VscError />
+          </span>
+        ))}
+      </aside>
+    </div>
+  );
+};
+
+export default Cart;
--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { Link } from "react-router-dom"
+import ProductCard from "../components/ProductCard"
 
 const Home = () => {
+  const addToCardHandler=()=>{}
   return (
-    <div>
-      Home
+    <div className="home">
+      <section></section>
+      <h1>Latest Product
+        <Link to={"/search"} className="findmore">More</Link>
+      </h1>
+      <main>
+        <ProductCard productId="abc" name="Macbook" price={232} stock={12}
+        handler={addToCardHandler}
+        photo="https://m.media-amazon.com/images/I/61Izntd7yML._AC_SX450_.jpg"/>
+      </main>
     </div>
   )
 }
